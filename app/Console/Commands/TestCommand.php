a925a0a47cc70adf2c851db20dcb45ed321d9521
Firdavs Ibodullaev <firdavs3253@gmail.com>
2022-04-23 15:21:34 +0500

    added test command

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Modules\Telegram\Telegram;
+use Illuminate\Console\Command;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bot:send';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $start = microtime(true);
+        $this->info($start);
+
+        $telegram = new Telegram();
+        $i = 0;
+        while ($i < 1000) {
+            $i++;
+            $telegram->send('sendMessage', [
+                'chat_id' => 287956415,
+                'text' => "Test Message number: {$i}"
+            ]);
+        }
+
+        $difference = number_format((microtime(true) - $start) * 1000, 2);
+        $telegram->send('sendMessage', [
+            'chat_id' => 287956415,
+            'text' => "Duration: {$difference}"
+        ]);
+        return 0;
+    }
+}
